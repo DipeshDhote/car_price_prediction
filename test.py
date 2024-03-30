0ab8a24e293b6ac6cf7da0df55ec753a04013fc5
Dipesh Dhote <dpsh.dhote@gmail.com>
2024-03-31 00:08:11 +0530

    add file

---
+++ test.py
@@ -0,0 +1,17 @@
+from src.car_price_prediction.logger import logging
+from src.car_price_prediction.exception import CustomException
+from src.car_price_prediction.data_ingestion import DataIngestion
+from src.car_price_prediction.components.data_ingestion import DataIngestionConfig
+import sys
+
+if __name__== "__main__":
+    logging.info("The excection has started")
+
+    try:
+        # Data Ingestion
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
+    except Exception as e:
+        logging.info("Custom exception")
+        raise CustomException(e,sys)
